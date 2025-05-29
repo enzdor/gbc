baf5d0f5971709bbe6ac2b13e6b78a9fe53159fb
enzdor <enzo01lb@gmail.com>
2025-05-29 15:22:32 -0300

    try to stop using variable length arrays

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## TODO
 
-- add shallow decision trees (depth = 5) or regression trees as weak learners instead of stump
+- add shallow decision trees (depth = 5) or regression trees as weak learners instead of stump, done?
 - regularize data?
 - do boosting with more than one feature
+- see if i really need variable length arrays
--- main.c
+++ main.c
@@ -7,11 +7,15 @@
 */
 
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+
+#define TRAIN_TIME 2
+#define DEPTH 5 // number of stumps
 
 typedef struct {
     float* values;
@@ -30,6 +34,10 @@ struct input_data {
 
 void init_array(Darray* arr, size_t initial_size)
 {
+    if (initial_size < 1) {
+        initial_size = 1;
+    }
+
     arr->values = malloc(initial_size * sizeof(float));
     arr->occupied = 0;
     arr->capacity = initial_size;
@@ -37,9 +45,7 @@ void init_array(Darray* arr, size_t initial_size)
 
 void add_element(Darray* arr, float element)
 {
-    if (arr->capacity == 0) {
-        arr->capacity = 1;
-    } else if (arr->occupied >= arr->capacity) {
+    if (arr->occupied >= arr->capacity) {
         arr->capacity *= 2;
         arr->values = realloc(arr->values, arr->capacity * sizeof(*arr->values));
     }
@@ -59,6 +65,32 @@ void delete_element(Darray* arr, float element)
             break;
         }
     }
+}
+
+int get_n_columns(char* line)
+{
+	int n_cols = 0;
+    char* tok;
+
+    for (tok = strtok(line, ","); tok && *tok; tok = strtok(NULL, ",\n")) {
+		n_cols++;
+    }
+
+	return n_cols;
+}
+
+int get_n_rows(FILE* fp)
+{
+	char* line;
+	size_t line_len;
+	int rows = 1;
+
+    while (getline(&line, &line_len, fp) != -1) {
+		rows++;
+    }
+
+	free(line);
+	return rows;
 }
 
 const char* get_csv_element(char* line, int num)
@@ -94,7 +126,25 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    float y_sum = 0, x_sum = 0, y_mean, x_mean, x_l_mean, x_h_mean, learning_rate;
+    char* line = NULL;
+    size_t line_len;
+
+	int err = getline(&line, &line_len, fp);
+	if (err == -1) {
+		puts("getline");
+		return 1;
+	}
+
+	// first col is y second is x
+	int cols = get_n_columns(line);
+	printf("%d cols\n", cols);
+
+	// number of observations
+	int rows = get_n_rows(fp);
+	printf("%d rows\n", rows);
+
+    float y_sum = 0, x_sum = 0, y_mean, x_mean, learning_rate;
+	// x_l_mean, x_h_mean,
     learning_rate = 0.8;
     // columns are x and y
 
@@ -106,9 +156,6 @@ int main(int argc, char* argv[])
     init_array(&inn.old_y, 0);
     init_array(&inn.residuals, 0);
     init_array(&inn.residuals_predictions, 0);
-
-    char* line = NULL;
-    size_t line_len;
 
     while (getline(&line, &line_len, fp) != -1) {
         for (int i = 1; i <= 2; i++) {
@@ -149,16 +196,7 @@ int main(int argc, char* argv[])
         inn.residuals.values[i] = inn.old_y.values[i] - inn.y.values[i];
     }
 
-    int left_count = 0, right_count = 0;
-    for (int i = 0; i < inn.og_y.occupied; i++) {
-        if (inn.x.values[i] < x_mean) {
-            left_count++;
-        } else {
-            right_count++;
-        }
-    }
-
-    time_t secs = 600 * 6;
+    time_t secs = TRAIN_TIME;
     time_t start_time = time(NULL);
 
     while (time(NULL) - start_time < secs) {
@@ -173,29 +211,155 @@ int main(int argc, char* argv[])
             }
         }
 
-        float left_pred = (left_count > 0) ? left_sum / left_count : 0;
-        float right_pred = (right_count > 0) ? right_sum / right_count : 0;
+        // float left_pred = (left_count > 0) ? left_sum / left_count : 0;
+        // float right_pred = (right_count > 0) ? right_sum / right_count : 0;
 
-        for (int i = 0; i < inn.og_y.occupied; i++) {
-            if (inn.x.values[i] < x_mean) {
-                inn.residuals_predictions.values[i] = left_pred;
-            } else {
-                inn.residuals_predictions.values[i] = right_pred;
-            }
+        int max_leaves = round(pow(2, DEPTH));
+        Darray save_leaves[DEPTH][max_leaves];
+        float save_means[DEPTH][max_leaves];
 
-            inn.old_y.values[i] = inn.y.values[i];
-            inn.y.values[i] = inn.y.values[i] + inn.residuals_predictions.values[i] * learning_rate;
-            if (i == 1) {
-		//               printf("y is %f and real y %f\n", inn.y.values[i], inn.og_y.values[i]);
-		// printf("preds %f and %f\n", left_pred, right_pred);
+        for (int depth = 0; depth < DEPTH; depth++) {
+            for (int j = 0; j < round(pow(2, depth + 1)); j++) {
+                init_array(&save_leaves[depth][j], 0);
             }
-            inn.residuals.values[i] = inn.old_y.values[i] - inn.y.values[i];
         }
+
+        save_leaves[0][0] = inn.x;
+
+
+        for (int depth = 1; depth < DEPTH; depth++) {
+            int l = round(pow(2, depth));
+
+            Darray leaves[l];
+
+            for (int i = 0; i < l; i++) {
+                init_array(&leaves[i], 0);
+            }
+
+            // for the number of leafs in x depth
+            for (int n_leaves = 0; n_leaves < l; n_leaves++) {
+                // find mean
+                float leaf_sum = 0, leaf_mean = 0;
+
+                for (int i = 0; i < save_leaves[depth - 1][n_leaves].occupied; i++) {
+                    leaf_sum += save_leaves[depth - 1][n_leaves].values[i];
+                }
+
+                leaf_mean = leaf_sum / save_leaves[depth - 1][n_leaves].occupied;
+                save_means[depth - 1][n_leaves] = leaf_mean;
+
+                // and split elements for the next depth
+                for (int i = 0; i < save_leaves[depth - 1][n_leaves].occupied; i++) {
+                    int leaf_pos = (n_leaves + 1) * 2 - 1;
+
+                    if (save_leaves[depth - 1][n_leaves].values[i] < leaf_mean) {
+                        // left branch
+                        add_element(&save_leaves[depth][leaf_pos - 1], save_leaves[depth - 1][n_leaves].values[i]);
+                    } else {
+                        // right branch
+                        add_element(&save_leaves[depth][leaf_pos], save_leaves[depth - 1][n_leaves].values[i]);
+                    }
+                }
+            }
+        }
+
+        float save_pseudo_residuals[inn.x.occupied];
+
+        // for each x
+        for (int i = 0; i < inn.x.occupied; i++) {
+            int end, current_leaf = 0;
+            float current_mean = 0;
+
+            for (int depth = 0; depth < DEPTH; depth++) {
+                int right_next_depth = current_leaf * 2 + 1;
+				printf("occupied %lu\n", save_leaves[depth][current_leaf].occupied);
+                // check if there is a mean in this leaf
+                if (save_leaves[depth][current_leaf].occupied < 1) {
+                    break;
+                }
+
+                current_mean = save_means[depth][current_leaf];
+
+                // save the pseudo residuals
+                if (inn.x.values[i] < save_means[depth][current_leaf]) {
+                    current_leaf = right_next_depth - 1;
+                } else {
+                    current_leaf = right_next_depth;
+                }
+            }
+
+            save_pseudo_residuals[i] = current_mean;
+			printf("%f save pseudo-residual, mean %f , current mean %f\n", save_pseudo_residuals[0], save_means[0][0], current_mean);
+			return 0;
+        }
+
+        /*
+         *
+         *
+         *  CALCULATE MEANS FOR EACH LEAF
+         *  pseudo residual
+         *
+         *
+         */
+
+        /*
+         *
+         *
+         * 	FOR EACH Y GO DOWN TREE ACCORDING TO THEIR X
+         * 	AND CALCULATE NEW Y ACCORDING TO THE MEAN OF THE
+         * 	LEAF AND THE LEARNING RATE
+         *
+         * 	Y_SUB_i+1 = Y_SUB_i + learning rate * pseudo residual
+         *
+         *
+         */
+
+        // once i have the mean for all of the leafs
+        // 		set the new y to the corresponding leaf by going down the tree
+        // 		calculate residuals mean
+        // 		set new y
+
+        // for (int depth = 1; depth <= DEPTH; depth++) {
+        //     Darray leaves[2];
+        //     Darray next_leaves[(depth + 1) * 2];
+        //     if (depth == 1) {
+        //     }
+        //     for (int i = 0; i < inn.og_y.occupied; i++) {
+        //         float left_sum = 0, right_sum = 0;
+        //
+        //         for (int i = 0; i < inn.og_y.occupied; i++) {
+        //             if (inn.x.values[i] < x_mean) {
+        //                 left_sum += inn.residuals.values[i];
+        //             } else {
+        //                 right_sum += inn.residuals.values[i];
+        //             }
+        //         }
+        //
+        //         float left_pred = (left_count > 0) ? left_sum / left_count : 0;
+        //         float right_pred = (right_count > 0) ? right_sum / right_count : 0;
+        //
+        //         Darray left_leaf, right_leaf;
+        //
+        //         init_array(&left_leaf, 0);
+        //         init_array(&right_leaf, 0);
+        //     }
+        // }
+
+        // for (int i = 0; i < inn.og_y.occupied; i++) {
+        //     if (inn.x.values[i] < x_mean) {
+        //         inn.residuals_predictions.values[i] = left_pred;
+        //     } else {
+        //         inn.residuals_predictions.values[i] = right_pred;
+        //     }
+        //
+        //     inn.old_y.values[i] = inn.y.values[i];
+        //     inn.y.values[i] = inn.y.values[i] + inn.residuals_predictions.values[i] * learning_rate;
+        //     inn.residuals.values[i] = inn.old_y.values[i] - inn.y.values[i];
+        // }
     }
 
     for (int i = 0; i < inn.og_y.occupied; i++) {
         printf("real y %f\n", inn.og_y.values[i]);
         printf("predicted y %f and old y %f\n", inn.y.values[i], inn.old_y.values[i]);
     }
-    printf("%lu\n", inn.og_y.occupied);
 }
